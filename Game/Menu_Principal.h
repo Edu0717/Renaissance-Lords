a0309db90542968c76d2a80d0959b9a28f861de1
Edu0717 <141575753+Edu0717@users.noreply.github.com>
2024-11-04 19:21:22 -0300

    Imprlementação do botão de saida do jogo

--- Game/Menu_Principal.h
+++ Game/Menu_Principal.h
@@ -6,13 +6,14 @@ using namespace sf;
 using namespace std;
 
 constexpr auto MENU_MAX_OPC = 4;
+constexpr auto CONFIRM_MAX_OPC = 2;
+
 
 void menu_inicial(RenderWindow& window_menu,int opc_select) {
 	Font fonte_menu;
 	Text opc_menu[4];
 	Texture background_tmenu;
 	Sprite background_smenu;
-	Image icon;
 	Color cor_opc(22, 8, 2);
 	Texture tmenu_select;
 	Sprite smenu_select;
@@ -25,7 +26,7 @@ void menu_inicial(RenderWindow& window_menu,int opc_select) {
 	if (!tmenu_select.loadFromFile("Assets/image/livro.png")) {
 		cerr << "Erro ao carregar indicador do menu!" << endl;
 	}
-	int i = 0;
+	
 	smenu_select.setTexture(tmenu_select);
 	smenu_select.setScale(0.1f, 0.1f);
 	
@@ -40,6 +41,8 @@ void menu_inicial(RenderWindow& window_menu,int opc_select) {
 		smenu_select.setPosition(15, 236 + (opc_select * 70)); // Posição ao lado da opção
 		window_menu.draw(smenu_select);
 	}
+	
+
 	for (int i = 0; i < MENU_MAX_OPC; ++i) {
 		opc_menu[i].setFont(fonte_menu);
 		opc_menu[i].setFillColor(cor_opc);
@@ -54,5 +57,65 @@ void menu_inicial(RenderWindow& window_menu,int opc_select) {
 		}
 		window_menu.draw(opc_menu[i]);
 	}
+}
+
+void confirma_saida(RenderWindow& window_menu, int sair_opc) {
+	Font fonte_confirm;
+	Text opc_confirm[2];
+	Text titulo_confirm;
+	Texture tconfirm_tela;
+	Sprite sconfirm_tela;
+	Texture tconfirm_select;
+	Sprite sconfirm_select;
+	Color confirm_color_opc(255,255,255);
+
+
+	if (!tconfirm_tela.loadFromFile("Assets/image/provisorio.png")) {
+		cerr << "Erro ao carregar tela de confirmação!" << endl;
+	}
+	sconfirm_tela.setTexture(tconfirm_tela);
+	sconfirm_tela.setPosition(225, 225);
+	window_menu.draw(sconfirm_tela);
+
+	if (!fonte_confirm.loadFromFile("Assets/fonts/GothicPixels.ttf")) {
+		cerr << "Erro ao carregar fonte!" << endl;
+	}
+
+	titulo_confirm.setFont(fonte_confirm);
+	titulo_confirm.setString("Voce tem certeza?");
+	titulo_confirm.setFillColor(confirm_color_opc);
+	titulo_confirm.setCharacterSize(32);
+	titulo_confirm.setPosition(250, 245);
+	window_menu.draw(titulo_confirm);
+	
+	for (int i = 0; i < CONFIRM_MAX_OPC; i++) {
+		opc_confirm[i].setFont(fonte_confirm);
+		opc_confirm[i].setFillColor(confirm_color_opc);
+		opc_confirm[i].setCharacterSize(36);
+		opc_confirm[i].setPosition(300 + (i * 125), 300.0);
+		switch (i) {
+		case 0: opc_confirm[i].setString("Sim"); break;
+		case 1: opc_confirm[i].setString("Nao"); break;
+		}
+		window_menu.draw(opc_confirm[i]);
+		
+	}
+	if (!tconfirm_select.loadFromFile("Assets/image/livro.png")) {
+		cerr << "Erro ao carregar indicador do menu!" << endl;
+	}
+	
+	sconfirm_select.setTexture(tconfirm_select);
+	sconfirm_select.setScale(0.09f, 0.09f);
+
+	if (sair_opc >= 0 && sair_opc < CONFIRM_MAX_OPC) {
+		sconfirm_select.setPosition(250 + (sair_opc * 125), 300); // Posição ao lado da opção
+		window_menu.draw(sconfirm_select);
+	}
+
+}
+
+void creditos_menu(RenderWindow& window_menu) {
+
+
 }
 
